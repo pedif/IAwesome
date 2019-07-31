1a4224f42a394b3d768bc1ddf0829f594db18948
Pedram <foroughi.pedram@gmail.com>
2019-07-31 16:51:48 +0300

    Add Awesome Recycler Adapter
    Added a adapter for a recycler view which is going to display awesome items

---
+++ app/src/main/java/com/techspark/iawesome/view/AwesomeRecyclerAdapter.kt
@@ -0,0 +1,30 @@
+package com.techspark.iawesome.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.techspark.iawesome.R
+import com.techspark.iawesome.database.AwesomeModel
+import kotlinx.android.synthetic.main.item_awesome.view.*
+
+class AwesomeRecyclerAdapter(private var items: List<AwesomeModel>) : RecyclerView.Adapter<AwesomeRecyclerAdapter.ViewHolder>() {
+
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_awesome,parent,false))
+    }
+
+    override fun getItemCount(): Int {
+
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        holder.itemView.item_awesome_text.text = items[position].msg
+    }
+
+}
