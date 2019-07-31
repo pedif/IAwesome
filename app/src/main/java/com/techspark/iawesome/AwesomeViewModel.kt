23b9dd5d0ee93eaf0b7ad9417b794bcf0fbfee10
Pedram <foroughi.pedram@gmail.com>
2019-07-31 16:25:30 +0300

    Update Awesome ViewModel
    Updated Viewmodel to start the workmanager with awesome worker task

--- app/src/main/java/com/techspark/iawesome/AwesomeViewModel.kt
+++ app/src/main/java/com/techspark/iawesome/AwesomeViewModel.kt
@@ -2,7 +2,20 @@ package com.techspark.iawesome
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import com.techspark.iawesome.database.AwesomeDatabase
+import java.text.DateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class AwesomeViewModel(application: Application) : AndroidViewModel(application) {
-    // TODO: Implement the ViewModel
+
+    
+
+    init {
+        val workManager = WorkManager.getInstance()
+        val periodicWorkRequest = PeriodicWorkRequest.Builder(AwesomeWorker::class.java, 60, TimeUnit.SECONDS).build()
+        workManager.enqueue(periodicWorkRequest)
+    }
 }
