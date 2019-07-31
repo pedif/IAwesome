af0424f2e1fbd1bf9389f5a4178b7f58e3f3c0a6
Pedram <foroughi.pedram@gmail.com>
2019-07-31 12:17:48 +0300

    Add Awesome Fragment
    Added awesome fragment which handles the viewing of the app

---
+++ app/src/main/java/com/techspark/iawesome/AwesomeFragment.kt
@@ -0,0 +1,32 @@
+package com.techspark.iawesome
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class AwesomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AwesomeFragment()
+    }
+
+    private lateinit var viewModel: AwesomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.awesome_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(AwesomeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
