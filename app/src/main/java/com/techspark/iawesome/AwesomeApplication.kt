1e218695e7f6fae162cdd80029f5e20d264f1da5
Pedram <foroughi.pedram@gmail.com>
2019-08-01 11:10:45 +0300

    Add Awesome Application
    Add awesome application which has the task to start and manage the workmanager

---
+++ app/src/main/java/com/techspark/iawesome/AwesomeApplication.kt
@@ -0,0 +1,19 @@
+package com.techspark.iawesome
+
+import android.app.Application
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import java.util.concurrent.TimeUnit
+
+class AwesomeApplication : Application() {
+
+    private val workTag = "AwesomeWork"
+    override fun onCreate() {
+        super.onCreate()
+
+        val workManager = WorkManager.getInstance()
+        val periodicWorkRequest = PeriodicWorkRequest.Builder(AwesomeWorker::class.java, 60, TimeUnit.SECONDS).build()
+        workManager.enqueueUniquePeriodicWork(workTag, ExistingPeriodicWorkPolicy.KEEP, periodicWorkRequest)
+    }
+}
