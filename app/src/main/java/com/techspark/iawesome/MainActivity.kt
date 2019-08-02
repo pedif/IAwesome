a01d2232e6382893586ff1adbb0bbe66c141a356
Pedram <foroughi.pedram@gmail.com>
2019-08-02 11:55:20 +0300

    Update Main Activity
    Updated activity to display main menue and respond to its actions

--- app/src/main/java/com/techspark/iawesome/MainActivity.kt
+++ app/src/main/java/com/techspark/iawesome/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.techspark.iawesome
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +12,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+         MenuInflater(this).inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.action_settings ->  startActivity(Intent(this,SettingsActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
