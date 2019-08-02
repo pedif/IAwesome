21db6405bf539ad4f10eb5820cfc67a1f4c809c2
Pedram <foroughi.pedram@gmail.com>
2019-08-02 12:28:37 +0300

    Update Main Activity
    Udpate main activity to check for gender  before starting

--- app/src/main/java/com/techspark/iawesome/MainActivity.kt
+++ app/src/main/java/com/techspark/iawesome/MainActivity.kt
@@ -2,27 +2,41 @@ package com.techspark.iawesome
 
 import android.content.Intent
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import java.util.prefs.Preferences
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkPref()
+    }
+
+    private fun checkPref() {
+
+        if (!PreferenceManager.getDefaultSharedPreferences(this).contains("gender"))
+            startSettingsActivity()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-         MenuInflater(this).inflate(R.menu.menu_main,menu)
+        MenuInflater(this).inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when(item?.itemId){
-            R.id.action_settings ->  startActivity(Intent(this,SettingsActivity::class.java))
+        when (item?.itemId) {
+            R.id.action_settings -> startSettingsActivity()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun startSettingsActivity() {
+
+        startActivity(Intent(this, SettingsActivity::class.java))
     }
 }
