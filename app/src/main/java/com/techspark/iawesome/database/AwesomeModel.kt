bdd31bc30d15c70ffa44c3cb72c4df7324b84226
Pedram <foroughi.pedram@gmail.com>
2019-08-04 10:48:49 +0300

    Update Awesome Model
    Updated date and time fields to have initial values

--- app/src/main/java/com/techspark/iawesome/database/AwesomeModel.kt
+++ app/src/main/java/com/techspark/iawesome/database/AwesomeModel.kt
@@ -3,6 +3,8 @@ package com.techspark.iawesome.database
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.DateFormat
+import java.util.*
 
 @Entity(tableName="awesome_model_table")
 class AwesomeModel {
@@ -14,10 +16,10 @@ class AwesomeModel {
             var msg = ""
 
         @ColumnInfo(name="date")
-            var date = ""
+            var date: String = DateFormat.getDateInstance().format(Date())
 
         @ColumnInfo(name="time")
-            var time=""
+            var time: String =DateFormat.getDateTimeInstance().format(Date())
 
 
 
